e699553bc79fafc7e5a0d82df2d290ab1cade9f4
Sarav <sarasoft@live.com>
2023-07-15 19:06:44 +0530

    Version2
    1) HTML report
    2) Recoverable/Non Recoverable Split
    3) Dryrun

--- README.md
+++ README.md
@@ -1,18 +1,77 @@
 ## Recover S3
 
-This script will recover files from S3 that have been deleted by
-removing the `deletemarker` from the S3 object.
+This script will recover files (objects) from Versioned S3 Buckets that have been soft deleted by
+removing their respective `deletemarker` 
 
-This is useful when you have a bucket with versioning enabled and you have deleted a file by mistake.
+To understand how RecoverS3 works,  you need to understand what is `deletemarker` 
 
-As of now, this script will only recover the files that have been deleted and not the files that have been overwritten.
 
-Also, this script will not recover the files that have been deleted and overwritten by another file.
+### What is S3 deletemarker and how it works
 
-we have added threading to speed up the process. As part of benchmarking, we have recovered 10000 files in 8 minutes.
+
+In Amazon S3 (Simple Storage Service), a "DeleteMarker" is a unique object that represents the deletion of a versioned object in a bucket. When versioning is enabled for an S3 bucket, each object uploaded to the bucket will have multiple versions, and when you delete an object, S3 doesn't remove it immediately. Instead, it inserts a `DeleteMarker` as the latest version of the object to indicate that the object has been deleted.
+
+Here's how DeleteMarkers work in S3:
+
+`Versioning:` Before DeleteMarkers can be used, you need to enable versioning for the S3 bucket. Once versioning is enabled, S3 will keep track of multiple versions of each object in the bucket.
+
+`Deletion:` When you delete an object in a versioned bucket, instead of physically removing the object, S3 inserts a new DeleteMarker object with the same key as the deleted object. The DeleteMarker's presence indicates that there was a previous version of the object that has been deleted.
+
+`Hiding the object:` When a DeleteMarker is in place, it acts as a placeholder for the deleted object, making it appear as if the object still exists. However, you won't be able to retrieve the object using regular GET requests.
+
+`List operations:` When listing objects in the bucket, S3 includes DeleteMarkers in the list along with the active versions of objects. This way, you can see that an object with a specific key was deleted and when it was deleted.
+
+`Permanent deletion:` If you want to permanently delete an object along with its versions (including the DeleteMarker), you need to use a specific delete operation called "Delete Object" with the object version ID. Deleting the object version containing the DeleteMarker will effectively remove all associated versions of that object, including the DeleteMarker itself.
+
+`S3 Lifecycle policies:` You can configure lifecycle policies in S3 to automatically remove older versions of objects and their corresponding DeleteMarkers after a specific duration.
+
+DeleteMarkers are essential for keeping track of the version history of objects in versioned S3 buckets. They provide a way to understand when and which versions of an object were deleted, allowing for more precise control over the data stored in the bucket.
+
+
+### How RecoverS3 deal with DeleteMarker
+
+As you have read and understood how `Deletion` and `DeleteMarker` and `Version Control` works on S3 bucket with Versioning enabled
+
+In a versioned bucket, when we delete an object/file it simply means we are adding a `Delete Marker` as the latest version of that document - in other words we refer it as a soft delete
+
+Now RecoverS3 tracks all these delete markers in the given S3 bucket matching the `prefix` ( directory structure) and validate if they are recoverable or not
+
+In a versioned S3 bucket. you can directly delete the non current version of the object, which could be the file itself. - We refer this as permanent deletion
+
+In this case, when a source file object is removed and delete marker is only present. we cannot recover that file
+
+So recoverS3 validates this scneario before starting the recovery
+
+If the source file does not exist, we cannot recover the file and it would be listed as `Non Recoverable` in the generated report.
+
+All other recoverables files are kept under `Recoverable` table 
+
+You can use the `-dryrun` option to generate a detailed report on what can be recovered and what cannot be , before proceeding with the actual recovery
+
+&nbsp;
+### Performance
+
+recoverS3 is built with threading model to speed up the process. As part of benchmarking, we have recovered 10000 files in 8 minutes.
 
 The speed of the script depends on the number of files that you are trying to recover, the network speed and the file size.
 
+&nbsp;
+
+### Searchable, Sortable Data report
+
+We have added a beautiful reporting feature into RecoverS3 and it generates a HTML report with `Bootstrap`, `datatable` and `Jquery` to make your report searchable, sortable and easy to find what you are looking for
+
+This would be a great help when you are dealing with large set of files and want to search for a file.
+
+The report contains the following fields
+
+* Recoverable files
+* Non Recoverable files
+* Total files
+
+
+
+&nbsp;
 
 ### Pre-requisites
 
@@ -22,6 +81,7 @@ The speed of the script depends on the number of files that you are trying to re
 * AWS Access Key and Secret Key
 * Permissions to access S3 bucket and list and delete objects
 
+&nbsp;
 
 ### Installation
 
@@ -44,13 +104,23 @@ optional arguments:
   --bucket BUCKET  S3 bucket name
   --prefix PREFIX  S3 prefix
   --region REGION  S3 region
+  --dryrun         Dry run
 ```
 &nbsp;
 
 #### Example
 
+- `For dry run` - This will list the files that will be recovered and creates a HTML report named report.html
+
 ```
-python recover-s3-files.py --bucket my-bucket --prefix my-prefix --region us-east-1
+python recover-s3-files.py --bucket my-bucket --prefix my-prefix --region us-east-1 --dryrun
+```
+
+- `For recovery` - This will recover the files and creates a HTML report named report.html
+
+
+```
+python recover-s3-files.py --bucket my-bucket --prefix my-prefix --region us-east-1 --dryrun
 ```
 &nbsp;
 
--- recover-s3-files.py
+++ recover-s3-files.py
@@ -7,12 +7,17 @@ import argparse
 import json
 import threading
 import time
+import pdb
 
 NextVersionIdMarker = None
 NextKeyMarker = None
 Repeat = True
 Results={'Success': 0 ,'Failed' : 0 }
-
+TotalFiles=[]
+TotalDeleteMarkers=[]
+RecoverableFiles=[] # Files with delete marker and source file present
+NonRecoverableFiles=[] # Files with delete marker but source file missing
+RecoverableFilesData=[] # Metadata of Recoverable Files
 
 class recoverfiles(threading.Thread):
     def __init__(self, S3obj, bucket, Key, VersionId):
@@ -65,10 +70,7 @@ def listdeletedversion():
         else:
             Repeat = False
 
-        for delete_marker in response['DeleteMarkers']:
-            print("INFO: Delete Marker found for Key {} with Version ID {} ".format(delete_marker['Key'],delete_marker['VersionId']))
-
-        return response['DeleteMarkers']
+        return response
     else:
         print("INFO: No delete markers found for prefix '{}' in bucket '{}'".format(args.prefix,args.bucket))
         return []
@@ -79,28 +81,269 @@ if __name__ == '__main__':
     parser.add_argument('--bucket', help='S3 bucket name', required=True)
     parser.add_argument('--prefix', help='S3 prefix', required=True)
     parser.add_argument('--region', help='S3 region', required=True)
+    parser.add_argument('--dryrun', help='Recovery would not be done in dryrun', required=False, action='store_true')
     args = parser.parse_args()
 
     S3 = boto3.client('s3', region_name=args.region)
    
     # call listdeletedversion to get all delete markers
 
+    iteration = 0
     while Repeat :
+        print("\nINFO: Iteration: {}".format(iteration))
         response = listdeletedversion()
         if response == []:
             Repeat = False
             break
         else:
+            TotalDeleteMarkers.extend(response['DeleteMarkers'])
+            TotalFiles.extend(response['Versions'])
             delete_markers = response
-            for delete_marker in delete_markers:
-                print("Starting thread for Key {} with Version ID {} ".format(delete_marker['Key'],delete_marker['VersionId']))
+            print("INFO: No of Delete markers found so far : {} in Iteration {}".format(len(TotalDeleteMarkers), iteration))
+            print("INFO: Saving delete Markers...")
+        iteration += 1
+
+    
+
+    print("\nINFO: Total Delete Markers found: {} for prefix {}".format(len(TotalDeleteMarkers),args.prefix))
+    # Some files might have multiple versions, so the total files found might be more than the total delete markers found.
+    print("INFO: Total Files found: {} for prefix {}".format(len(TotalFiles),args.prefix))
+    
+    
+    # Parse through the Total Files and Collect the Keys
+    TotalFilesKeys = []
+    for file in TotalFiles:
+        TotalFilesKeys.append(file['Key'])
+
+
+    for delete_marker in TotalDeleteMarkers:
+        ## 
+        # check if source file is present for the delete marker key
+        # sometimes, only delete marker is present for the key and the source file would be deleted 
+        # in that case, we cannot recover the file
+        # if source file is present, we can recover the file by deleting the delete marker
+        ## 
+        if str(delete_marker['Key']) in str(TotalFiles):
+            # print("INFO: Source file found for Key {} with Version ID {} ".format(delete_marker['Key'],delete_marker['VersionId']))
+            RecoverableFiles.append(delete_marker)
+            
+            # get the data from TotalFiles and add it to RecoverableFilesData using the map
+            RecoverableFilesData.append(list(filter(lambda x: x['Key'] == delete_marker['Key'], TotalFiles))[0])
+                        
+        else:
+            print("INFO: Source file not found for Key {} with Version ID {} ".format(delete_marker['Key'],delete_marker['VersionId']))
+            NonRecoverableFiles.append(delete_marker)
+
+    print("\nINFO: Total Recoverable Files: {}".format(len(RecoverableFiles)))
+    print("INFO: Total Non-Recoverable Files: {}".format(len(NonRecoverableFiles)))
+
+    # Save all the files as HTML table 
+    
+    with open('Report.html', 'w') as f:
+
+        # add bootstrap to the html file to make it look good
+        f.write('''
+            <html>
+            <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>
+            <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+            <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'></script>
+            <link rel='stylesheet' href='https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css'>
+            <script src='https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js'></script>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+            <script type="text/javascript">
+                    let table = new DataTable('#myTable', {
+                        responsive: true
+                    });
+                    $(document).ready( function () {
+                        $('#Summary').DataTable();
+                        $('#Recover').DataTable(
+                            {
+                                autoWidth: false,
+                                columns : [
+                                    { width : '50%' },
+                                    { width : '20%' },
+                                    { width : '20%' },
+                                    { width : '20%' },           
+                                ] 
+                            });
+                        $('#NonRecover').DataTable({
+                                autoWidth: false,
+                                columns : [
+                                    { width : '50%' },
+                                    { width : '20%' },
+                                    { width : '20%' },
+                                    { width : '20%' },           
+                                ] 
+                            });
+                        $('#Total').DataTable({
+                                autoWidth: false,
+                                columns : [
+                                    { width : '50%' },
+                                    { width : '20%' },
+                                    { width : '20%' },
+                                    { width : '20%' },           
+                                ] 
+                            });
+                    } );
+                </script>
+                ''')
+        f.write('''<style>
+        table.dataTable tbody td {
+            word-break: break-word;
+            vertical-align: top;
+        }
+        body{
+            background: rgb(240, 181, 223);
+            background: linear-gradient(90deg, rgba(240, 181, 223, 1) 0%, rgba(138, 187, 226, 1) 100%);
+            font-family: 'Open Sans', sans-serif;
+        }
+
+        .container {
+            background-color: white;
+            margin-top: 2% !important;
+            margin-bottom: 2% !important;
+            /* add shadow effect */
+            /* -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.75); */
+            -moz-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.75);
+            box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.75);
+        }
+
+        h1{
+            font-family: 'Ubuntu', sans-serif;
+        }
+
+        h2,
+        h3 {
+            color: black;
+            font-family: 'Ubuntu', sans-serif;
+            background-color: aliceblue;
+            font-weight: 800;
+            font-size: 24px;
+            padding: 1%;
+            margin-left: -2%;
+        }
+
+        .jumbotron {
+            padding-top: 10px !important;
+            padding-bottom: 10px !important;
+            margin-bottom: 30px;
+            color: inherit;
+            background-color: aliceblue;
+            margin-top: -5px;
+            width: 60%;
+            margin-left: -1%;
+            float: left !important;
+        }
+
+        code {
+            color: rgba(0, 0, 0, 0.37);
+            background-color: transparent;
+        }
+
+        .info {
+            margin-top: 2%;
+            margin-left: 2%;
+            margin-right: 2%;
+            float: right;
+        }
+        .tagline{
+            font-family: 'Ubuntu', sans-serif;
+            font-size: 16px !important;
+            color: rgba(0, 0, 0, 0.37) !important;
+        }
+        .footer{
+            display: flex;
+            padding: 2%;
+            margin-bottom: -1%;
+            margin-top: 3%;
+            background-color: aliceblue;
+            color: rgba(0, 0, 0, 0.37) !important;
+        }
+        .footer__right{
+            margin-left: 30%;
+        }
+    </style>''')
+        f.write("</head><body>")
+        f.write('''
+            <div class='container'>
+            <!-- jumbotron -->
+            <div class="row">
+                <div class="col">
+                    <div class='jumbotron' style="float: left">
+                        <h1>Recover<span style="font-weight: bold;">S3</span></h1>
+                        <p class="tagline">Recover deleted files from Versioned S3 bucket</p>
+                    </div>
+                </div>
+                <div class="col info">
+                        <p><img src='https://img.icons8.com/windows/42/github' />https://github.com/aksarav/recovers3.git</p>
+                        <!-- website link with icon -->
+                        <p><img src='https://img.icons8.com/windows/42/web'/>https://aksarav.github.io/recovers3/</p>
+                </div>
+            </div>
+                ''')
+        # add the summary table
+        f.write("<h3>Recover S3 - Summary</h3></br>")
+        f.write("<table id='Summary' class='display'><thead><th>Bucket</th><th>Prefix</th><th>Region</th><th>Recoverable Files</th><th>Non-Recoverable Files</th><th>Total Files</th><th>Total Delete Markers</th></thead>")
+        f.write("<tbody><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tbody>".format(args.bucket,args.prefix,args.region,len(RecoverableFiles),len(NonRecoverableFiles),len(TotalFiles),len(TotalDeleteMarkers)))
+        f.write("</table>")
+        f.write("<br><br>")
+
+        # add CSS to the html file to make it look good
+        #f.write("<style>table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 5px; text-align: left;}</style>")
+
+        
+        htmlfields="<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>"
+        f.write("<h3>Recoverable Files</h3><br>")
+        f.write("<table  id='Recover' class='display'><thead><th>Key</th><th>Version ID</th><th>Last Modified</th><th>Size</th></thead><tbody>")
+        for file in RecoverableFilesData:
+            f.write(htmlfields.format(file['Key'],file['VersionId'],file['LastModified'],file['Size']))
+        f.write("</tbody></table>")
+
+        f.write("<table  id='NonRecover' class='display'><thead><th>Key</th><th>Version ID</th></thead><tbody>")
+        f.write("<h3>Non Recoverable Files</h3><br>")
+        for file in NonRecoverableFiles:
+            f.write(htmlfields.format(file['Key'],file['VersionId']))
+        f.write("</tbody></table>")
+    
+        f.write("<table  id='Total' class='display'><thead><th>Key</th><th>Version ID</th><th>Last Modified</th><th>Size</th></thead><tbody>")
+        f.write("<h3>Total Files</h3><br>")
+        for file in TotalFiles:
+            f.write(htmlfields.format(file['Key'],file['VersionId'],file['LastModified'],file['Size']))
+        f.write("</tbody></table>")
+        f.write('''
+            <hr><footer>
+                        <div class="footer">
+                            <div class="footer__right">
+                                <div class="footer__right__text">
+                                    <p> For further reference visit <a href="https://www.recoverS3.com">devopsjunction.com/recovers3</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                ''')
+        f.write("</div></body></html>")
+    
+
+    
+    # Process delete markers
+    if not args.dryrun:
+        input("\nPress Enter to start the recovery process...")
+        print("INFO: Starting the recovery process...")
+        for delete_marker in RecoverableFiles:
+                #print("Starting thread for Key {} with Version ID {} ".format(delete_marker['Key'],delete_marker['VersionId']))
                 r = recoverfiles(S3, args.bucket, delete_marker['Key'], delete_marker['VersionId'])
                 threading.Thread(target=r.run).start()
                 while threading.active_count() > 10:
                     # sleep for a while until a thread is available
                     print("Waiting for a threads to finish")
                     time.sleep(1)
-
+    else:
+        print("INFO: Dry run enabled. Recovery process will not start")
+        sys.exit(0)
 
     # wait for all threads to finish
     while threading.active_count() > 1:
@@ -109,6 +352,5 @@ if __name__ == '__main__':
         print("Waiting for all threads to finish")
 
     print("INFO: All delete markers removed for prefix '{}' in bucket '{}'".format(args.prefix,args.bucket))
-
     print("INFO: Total Success: {} and Failed: {}".format(Results['Success'],Results['Failed']))
     sys.exit(0)
